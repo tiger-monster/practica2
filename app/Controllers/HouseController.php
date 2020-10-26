860c81e40cfe28bb17b45233b6e7099791e7abcf
tiger-monster <72220910+tiger-monster@users.noreply.github.com>
2020-10-26 16:41:57 -0600

    Add files via upload

---
+++ app/Controllers/HouseController.php
@@ -0,0 +1,60 @@
+<?php namespace App\Controllers;
+
+class HouseController extends BaseController
+{
+	public function index()
+	{
+
+		
+		$vistas = view('plantilla/header').
+					view('plantilla/menu').
+					view('index').
+					view('plantilla/footer');
+		return $vistas;
+	}
+
+	public function personajes()
+	{
+
+		
+		$vistas = view('plantilla/header').
+					view('plantilla/menu1').
+					view('personajes').
+					view('plantilla/footer');
+		return $vistas;
+	}
+	public function tem()
+	{
+
+		
+		$vistas = view('plantilla/header').
+					view('plantilla/menu1').
+					view('temporadas').
+					view('plantilla/footer');
+		return $vistas;
+	}
+
+	public function lib()
+	{
+
+		
+		$vistas = view('plantilla/header').
+					view('plantilla/menu1').
+					view('libros').
+					view('plantilla/footer');
+		return $vistas;
+	}
+
+	public function cur()
+	{
+
+		
+		$vistas = view('plantilla/header').
+					view('plantilla/menu1').
+					view('curiosidades').
+					view('plantilla/footer');
+		return $vistas;
+	}
+
+	
+}
